b59546af13b8b55f0eb3ed89816441f3e07dc665
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-07-20 12:23:08 +0200

    Add header bar to show kernel activity

--- share/jupyter/voila/templates/debug/nbconvert_templates/util.js
+++ share/jupyter/voila/templates/debug/nbconvert_templates/util.js
@@ -1,6 +1,25 @@
 window.debug_init = async (voila) => {
   const kernel = await voila.connectKernel();
   kernel.statusChanged.connect((sender, status) => {
-      $("#kernel-status").text(status)
+    switch (status) {
+      case "idle":
+        $("#kernel-activity").removeClass("jp-FilledCircleIcon").addClass("jp-CircleIcon");
+        $("#kernel-indicator").fadeOut(500);
+        break;
+      case "busy":
+        $("#kernel-activity").removeClass("jp-CircleIcon").addClass("jp-FilledCircleIcon");
+        $("#kernel-indicator").fadeOut(500);
+        break;
+      case "restarting":
+        $("#kernel-indicator").addClass("jp-mod-warn");
+        $("#kernel-indicator-label").text("Restarting");
+        $("#kernel-indicator").show();
+        break;
+      case "connected":
+        $("#kernel-indicator").removeClass("jp-mod-warn");
+        $("#kernel-indicator-label").text("Connected");
+        $("#kernel-indicator").show();
+        break;
+    }
   });
 };
